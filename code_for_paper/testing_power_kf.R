ba0b5b51d900edf962edd6364158edef2582f8e3
jfre0619@uni.sydney.edu.au <jfre0619@uni.sydney.edu.au>
2024-01-03 15:06:33 +1100

    update adakf results

--- code_for_paper/testing_power_kf.R
+++ code_for_paper/testing_power_kf.R
@@ -7,13 +7,14 @@ library(tidyverse)
 library(gam)
 source("filter_RF.R")
 source("filter_EM.R")
+source("filter_EM_full.R")
 
 #seed
 set.seed(1012024)
 
 #read in example data
-df = read_delim('datasets/PXD019354/crux-output/open_1_0.make-pin.pin')
-peptide_list = read_delim('datasets/PXD019354/index-0/tide-index.peptides.txt')
+df = read_delim('datasets/PXD022257/crux-output/open_1_0.make-pin.pin')
+peptide_list = read_delim('datasets/PXD022257/index-0/tide-index.peptides.txt')
 
 df = df[sample(nrow(df)), ]
 df = df[order(df$TailorScore, decreasing = TRUE), ]
@@ -23,37 +24,24 @@ df$original_target_sequence[df$Label == -1] = peptide_list$target[match(df$origi
 
 df = df[!duplicated(df$original_target_sequence), ]
 
-W = df$TailorScore*(df$Label)
 z = df %>% select('deltLCn', 'deltCn', 'Charge1', 'Charge2', 'Charge3', 'Charge4', 'Charge5',  'XCorr', 'PepLen', 'lnNumDSP', 'dM', 'absdM')
 z = as.matrix(z)
-
-props = (1:9)/10
-results = rep(0, length(props))
-for (i in 1:length(props)) {
-  print(i)
-  result = filter_RF(W, z, reveal_prop = props[i], mute = FALSE)
-  results[i] = result
-}
-
-mean_time = mean(results)
-
-#number of Percolator-RESET discoveries
-alpha = 0.01
-target_peps = read.delim('datasets/PXD019354/crux-output/testing_adaptive_kf.peptides.txt')
-decoy_peps = read.delim('datasets/PXD019354/crux-output/testing_adaptive_kf.decoy_peptides.txt')
-min_score = min(target_peps$SVM_score[target_peps$q_val <= alpha])
-targets = sum(target_peps$SVM_score >= min_score)
-decoys = sum(decoy_peps$SVM_score >= min_score)
-reset = targets + decoys
-
-print('Time for RF:')
-print(paste('average time in minutes:', mean_time*(nrow(df)*0.9 - reset) ))
-print(paste('sd time in minutes:', sd(results)*(nrow(df)*0.9 - reset) ))
 
 W = df$TailorScore
 W = W - min(W)
 W = W*(df$Label)
 W[abs(W) <= 1e-3] = 0
+
+#number of Percolator-RESET discoveries
+alpha = 0.01
+target_peps = read.delim('datasets/PXD022257/crux-output/testing_adaptive_kf.peptides.txt')
+decoy_peps = read.delim('datasets/PXD022257/crux-output/testing_adaptive_kf.decoy_peptides.txt')
+min_score = min(target_peps$SVM_score[target_peps$q_val <= alpha])
+targets = sum(target_peps$SVM_score >= min_score)
+decoys = sum(decoy_peps$SVM_score >= min_score)
+reset = targets + decoys
+
+props = (1:9)/10
 results = rep(0, length(props))
 for (i in 1:length(props)) {
   print(i)
@@ -66,4 +54,21 @@ mean_time = mean(results)
 print('Time for EM:')
 print(paste('average time in minutes:', mean_time*(nrow(df)*0.9 - reset) ))
 print(paste('sd time in minutes:', sd(results)*(nrow(df)*0.9 - reset) ))
+
+
+start = Sys.time()
+results = filter_EM_full(W, z, alpha = c(0.05, 0.04, 0.03, 0.02, 0.01), reveal_prop = 0.1, mute = FALSE)
+end = Sys.time()
+
+print('Actual time:')
+print(end - start)
+
+print('Rejections:')
+print(results$nrejs)
+
+#plot values
+adakf = rev(c(2888, 2763, 2668, 2462, 2264))
+df_RESET = read.delim('datasets/PXD022257/crux-output/testing_adaptive_kf.peptides.txt')
+reset = c(sum(df_RESET$q_val <= 0.01), sum(df_RESET$q_val <= 0.02), sum(df_RESET$q_val <= 0.03), sum(df_RESET$q_val <= 0.04), sum(df_RESET$q_val <= 0.05))
+
 
